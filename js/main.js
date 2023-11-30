09552d8929fc51cd468003271a6d8504da2c8a75
jharahul968 <jharahul968@gmail.com>
2023-11-30 15:58:04 +0545

    implement comment feature

--- js/main.js
+++ js/main.js
@@ -37,10 +37,13 @@ notif_btn.onclick = function () {
 //     }
 // }
 
+
+
 if (!localStorage.likes_count) {
     localStorage.setItem('likes_count', '0');
 }
-likes_count=Number(localStorage.getItem('likes_count'));
+
+likes_count = Number(localStorage.getItem('likes_count'));
 like_btn = document.getElementById('like_btn')
 like_btn.onclick = function () {
     likes_count += 1;
@@ -52,3 +55,52 @@ function putTexts() {
     document.getElementById('likes_count').innerHTML = likes_count;
 }
 
+
+
+
+function postComment() {
+    input = document.getElementById("comment_input");
+    comment = input.value;
+    if (comment.trim()!==''){
+    document.getElementById("comment_input").value = '';
+    comments_body = document.getElementById("comments_body");
+    new_comment = document.createElement("div");
+    new_comment.innerHTML = `
+<div class="comments_body--comment">
+<img src="assets/images/profile-icon.png" alt="commenter" />
+<p class="comments_body--commenter"><b>@commenter</b></p>
+<p>time of comment</p>
+</div>
+<div class="comment">
+<p>${comment}</p>
+</div>
+
+<div class="comment_actions_wrapper">
+<div class="comment_actions">
+  <button class="comment_button like_comment">
+    <img src="assets/images/video-buttons/like.svg" />
+  </button>
+  <p>Likes count</p>
+  <button class="comment_button dislike_comment">
+    <img src="assets/images/video-buttons/dislike.svg" />
+  </button>
+  <button class="comment_button reply_comment">
+    <b>Reply</b>
+  </button>
+</div>
+<div>
+  <button class="see_reply_button">
+    <img
+      src="assets/images/video-buttons/down.svg"
+      alt="down"
+    />
+    15 replies
+  </button>
+</div>
+</div>
+`
+
+comments_body.prepend(new_comment);
+console.log(comments_body);
+    }
+}
